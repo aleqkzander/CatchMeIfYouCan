841512ddf1243c24df7a8e620f93283787f84225
Alexander Lairich <alexanderlairich@gmx.net>
2024-01-06 23:15:16 +0100

    Refactoring the base player class

--- Catch Me If You Can/Assets/Game/Third Person Character/Scripts/PlayerStatistic.cs
+++ Catch Me If You Can/Assets/Game/Third Person Character/Scripts/PlayerStatistic.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class PlayerStatistic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private float _matchTime;
 }
