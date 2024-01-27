5bbe7093b9ccc383f77cb981664e8c1d0ad5eb9a
Alexander Lairich <alexanderlairich@gmx.net>
2024-01-27 06:32:28 +0100

    Integrated tutorial into playcube

--- Catch Me If You Can/Assets/Game/Menu Cubes/Play Cube/PlayCube.cs
+++ Catch Me If You Can/Assets/Game/Menu Cubes/Play Cube/PlayCube.cs
@@ -1,14 +1,16 @@
 using Mirror;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayCube : MonoBehaviour
 {
     public Image ScenePreview;
+    public TMP_Text HostGameTxt;
     public TMP_Text SceneName;
     public TMP_InputField IpInput;
-    private int currentScene = 1;
+    private int currentScene = 0;
 
     private void Start()
     {
@@ -30,12 +32,13 @@ public class PlayCube : MonoBehaviour
 
         if (currentScene < maxScenes - 1)
         {
+            HostGameTxt.text = "HOST GAME";
             currentScene++;
         }
         else
         {
-            // keep currentScene 1 to exclude the Playground scene
-            currentScene = 1;
+            HostGameTxt.text = "PLAY TUTORIAL";
+            currentScene = 0;
         }
 
         DisplayCurrentLevel(currentScene);
@@ -43,7 +46,14 @@ public class PlayCube : MonoBehaviour
 
     public void Cube_StartHost()
     {
-        NetworkManager.singleton.StartHost();
+        if (currentScene == 0)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            NetworkManager.singleton.StartHost();
+        }
     }
 
     public void Cube_StartClient()
